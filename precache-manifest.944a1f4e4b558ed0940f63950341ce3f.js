21dba37b8aee6eb86b9f30cf7509a9bb85c9697c
Nisarg <nisargparab8691@gmail.com>
2022-09-18 18:29:14 +0530

    Updates

---
+++ precache-manifest.944a1f4e4b558ed0940f63950341ce3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "597a93f25ed7a917eead",
+    "url": "/text-utils/static/css/main.172008c7.chunk.css"
+  },
+  {
+    "revision": "597a93f25ed7a917eead",
+    "url": "/text-utils/static/js/main.597a93f2.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/text-utils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/text-utils/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "eef656731becf2616f54",
+    "url": "/text-utils/static/js/runtime~main.eef65673.js"
+  },
+  {
+    "revision": "3ec3a38ed14551e5098a71a3d8e77be5",
+    "url": "/text-utils/index.html"
+  }
+];
